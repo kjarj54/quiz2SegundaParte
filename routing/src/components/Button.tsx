6593c060f1ce4954d0786fe0554103afaa1384ee
Anthony Avila <105742243+Anthonyah131@users.noreply.github.com>
2024-04-10 21:49:52 -0600

    Correcion bug

--- routing/src/components/Button.tsx
+++ routing/src/components/Button.tsx
@@ -1,4 +1,4 @@
-const Button = ({ id, color, children, onClick }) => (
+const Button = ({ id, color, children, onClick }: { id: string, color: string, children: React.ReactNode, onClick: () => void }) => (
   <button
     className={`px-4 py-2 rounded block w-full text-white bg-${color}-500 hover:bg-${color}-600`}
     id={id}
